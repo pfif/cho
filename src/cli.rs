5e510e8b3c68627ad2c8e15050016345da662abf
fpastor <fpastor@pop-os.localdomain>
2024-03-13 21:55:59 +0900

    Design for the "remaining" screen

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,30 @@ use crate::remaining::remaining_money;
 use crate::vault::get_vault_value;
 use chrono::NaiveDate;
 
+
+/*
+Period start: 2023/02/01
+============
+
+Accounts
+========
+             | ING       | Credit Mutuel     | Wise            | LINE            | ゆうちょ            | Liquide      | Total
+Period start | €0        | €54775.19         | €2889.14        | ¥24796          | ¥3758343            | ¥13000       | €81518.52
+Current      | €0 (-€0)  | €54000.00 (-€547) | €3000 (+€28)    | ¥22300 (-¥796)  | ¥3266780 (-¥491563) | ¥12987 (-¥13)| €79518.52 (-€2000)
+
+(+) Predicted Income: €2000
+====================
+
+(-) Goals
+=========
+               | Commited | Commited this period | Target |
+Retirment fund | €15000   | … (€500)             | €60000 |
+New iPhone     | €300     | ✅                   | €1000  |
+Total          | €15300   | €2000 (€2500)        | €61000 |
+
+Remaining this period: €456
+*/
+
 fn remaining() {
     // TODO Parse arguments
     let exchange_rate: ((String, f64), (String, f64));
