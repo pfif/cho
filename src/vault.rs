921e52b4ebd16bb63ecee7145b70df87691cad24
fpastor <fpastor@pop-os.localdomain>
2024-04-15 08:57:30 +0900

    Started the vault

--- src/vault.rs
+++ src/vault.rs
@@ -1,7 +1,18 @@
-/*use crate::period::PeriodsConfiguration;
+use crate::goals::PeriodsConfiguration;
+use std::path::PathBuf;
+use std::fs::{File};
 use serde::Deserialize;
+use serde_json::from_reader;
+use std::any::Any;
 
-#[derive(Deserialize)]
-pub struct VaultValues{
-    period: PeriodsConfiguration
-}*/
+pub struct Vault {
+    path: PathBuf
+}
+
+impl Vault {
+    pub fn read_vault_values<'a, T: Deserialize<'a>>(&self, name: String) -> Result<T, String>{
+        let path = self.path.join("config.json");
+        let config = File::open(path).and_then(from_reader).map(|x| x[name]);
+    }
+}
+
