abd3d213dccb3737f01e2d05002feda372bcede8
pfif <florent.pastor@gmail.com>
2024-06-04 00:03:03 +0900

    Good ol' cargo fmt

--- src/period/mod.rs
+++ src/period/mod.rs
@@ -1,10 +1,10 @@
-mod periods_configuration;
-mod fixed_length_period;
 mod calendar_month_period;
+mod fixed_length_period;
 mod periods;
+mod periods_configuration;
 
-pub use periods_configuration::{AnyPeriodsConfiguration, PeriodsConfiguration};
 pub use periods::Period;
+pub use periods_configuration::{AnyPeriodsConfiguration, PeriodsConfiguration};
 
 #[cfg(test)]
 pub use periods_configuration::MockPeriodsConfiguration;
