f3627dd50c0c3c8c4ad140d7c44f630b6bf40478
pfif <florent.pastor@gmail.com>
2025-04-20 18:29:17 +0900

    Added a TODO

--- src/remaining_operation/mod.rs
+++ src/remaining_operation/mod.rs
@@ -5,3 +5,4 @@ pub mod operand_builders;
 // TODO Remove the TODOs before merging
 // TODO see if there isn't a better way to organize the code
 // TODO work on the documentation a bit
+// TODO delete the now unused code
