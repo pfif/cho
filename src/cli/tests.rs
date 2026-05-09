4eb80287c1dd3286a265b378661866cc91a653cc
pfif <florent.pastor@gmail.com>
2026-05-09 16:41:10 +0900

    remove the useless test table from format_remaining_operation_screen_tests

--- src/cli/tests.rs
+++ src/cli/tests.rs
@@ -1,12 +1,10 @@
 #[cfg(test)]
 mod format_remaining_operation_screen_tests {
-    use std::collections::HashMap;
     use chrono::NaiveDate;
     use rust_decimal_macros::dec;
     use crate::cli::formatting::format_remaining_operation_screen;
     use crate::period::Period;
     use pretty_assertions::assert_eq;
-    use crate::amounts::{Amount, Currency};
     use crate::amounts::exchange_rates::ExchangeRates;
     use crate::remaining_operation::core_types::group::Group;
     use crate::remaining_operation::core_types::{Illustration, IllustrationValue, Operand, RemainingOperationScreen};
@@ -33,63 +31,42 @@ mod format_remaining_operation_screen_tests {
         }
     }
 
-    struct TestTable {
-        include_empty_group: bool,
-        include_normal_group: bool,
-        include_extra_column_group: bool,
-        
-        expected_output: String
-    }
-    impl TestTable {
-        fn test(self) {
-            let date = NaiveDate::from_ymd_opt(2026,5, 9).expect("Can create date");
-            let exchange_rates = ExchangeRates::for_tests();
-
-            let mut groups = vec![];
-            if self.include_empty_group {
-                let empty_group = Group::new("Empty".into(), vec![]).expect("Could make group");
-                groups.push(empty_group.into_remaining_operation_screen_group(&exchange_rates, &"EUR".to_string(), &date).expect("Could make group"));
-            }
-             
-            if self.include_normal_group {
-                let normal_group = Group::new("Normal group".into(), vec![
-                    make_operand(&exchange_rates, "Payment for house".into(), false, false),
-                    make_operand(&exchange_rates, "Payment for dog".into(), false, false),
-                    make_operand(&exchange_rates, "Payment for cat".into(), false, true)
-                ]).expect("Could make group");
-                groups.push(normal_group.into_remaining_operation_screen_group(&exchange_rates, &"EUR".to_string(), &date).expect("Could make group"));
-            }
-            
-            if self.include_extra_column_group {
-                let extra_column_group = Group::new("Extra column group".into(), vec![
-                make_operand(&exchange_rates, "Payment for Mr Spock".into(), true, false),
-                make_operand(&exchange_rates, "Payment for Jean Luc".into(), true, false),
-                make_operand(&exchange_rates, "Payment for Katherine".into(), true, false)
-                ]).expect("Could make group");
-
-                groups.push(extra_column_group.into_remaining_operation_screen_group(&exchange_rates, &"EUR".to_string(), &date).expect("Could make group"));
-            }
-            
-            let screen = RemainingOperationScreen{
-                groups,
-                remaining: exchange_rates.new_amount(&"EUR".to_string(), dec!(100)).expect("Could create amount"),
-                period: Period{
-                    start_date: NaiveDate::from_ymd_opt(2025,1,1).unwrap(),
-                    end_date: NaiveDate::from_ymd_opt(2025, 1, 31).unwrap()
-                },
-            };
-            
-            assert_eq!(format_remaining_operation_screen(&screen), self.expected_output)
-        }
-    }
-    
     #[test]
-    fn test_all_groups(){
-        TestTable{
-            include_empty_group: true,
-            include_normal_group: true,
-            include_extra_column_group: true,
-            expected_output: r#"Current period : 2025-01-01 to 2025-01-31
+    fn test() {
+        let date = NaiveDate::from_ymd_opt(2026,5, 9).expect("Can create date");
+        let exchange_rates = ExchangeRates::for_tests();
+
+        let mut groups = vec![];
+        let empty_group = Group::new("Empty".into(), vec![]).expect("Could make group");
+        groups.push(empty_group.into_remaining_operation_screen_group(&exchange_rates, &"EUR".to_string(), &date).expect("Could make group"));
+         
+        let normal_group = Group::new("Normal group".into(), vec![
+            make_operand(&exchange_rates, "Payment for house".into(), false, false),
+            make_operand(&exchange_rates, "Payment for dog".into(), false, false),
+            make_operand(&exchange_rates, "Payment for cat".into(), false, true)
+        ]).expect("Could make group");
+        groups.push(normal_group.into_remaining_operation_screen_group(&exchange_rates, &"EUR".to_string(), &date).expect("Could make group"));
+        
+        let extra_column_group = Group::new("Extra column group".into(), vec![
+        make_operand(&exchange_rates, "Payment for Mr Spock".into(), true, false),
+        make_operand(&exchange_rates, "Payment for Jean Luc".into(), true, false),
+        make_operand(&exchange_rates, "Payment for Katherine".into(), true, false)
+        ]).expect("Could make group");
+
+        groups.push(extra_column_group.into_remaining_operation_screen_group(&exchange_rates, &"EUR".to_string(), &date).expect("Could make group"));
+        
+        
+        let screen = RemainingOperationScreen{
+            groups,
+            remaining: exchange_rates.new_amount(&"EUR".to_string(), dec!(100)).expect("Could create amount"),
+            period: Period{
+                start_date: NaiveDate::from_ymd_opt(2025,1,1).unwrap(),
+                end_date: NaiveDate::from_ymd_opt(2025, 1, 31).unwrap()
+            },
+        };
+        
+        // TODO add a test to see the formatting of a group with an archived operand that should not be archived
+        assert_eq!(format_remaining_operation_screen(&screen), r#"Current period : 2025-01-01 to 2025-01-31
 =========================================
 
 Empty
@@ -127,7 +104,6 @@ Extra column group
 Remaining this period: €100
 =============================
 
-Release: Development build"#.to_string(),
-        }.test()
+Release: Development build"#.to_string())
     }
 }
